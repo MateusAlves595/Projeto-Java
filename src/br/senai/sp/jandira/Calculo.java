3c9bfe9d2496803b4a235c8b396f57c63b563bbe
Mateus Alves <22282205@CFP127ALUNO.LOCAL>
2022-09-01 16:42:32 -0300

    Tabuada

---
+++ src/br/senai/sp/jandira/Calculo.java
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira;
+
+public class Calculo {
+
+	public int multiplicador;
+	public int minimo;
+	public int maximo;
+	
+	
+	public void getCalculo () {
+		
+	}
+	
+
+	
+}
