959a0091cd715f18fe091ce2bf100156647e6ddc
Mateus Alves <22282205@CFP127ALUNO.LOCAL>
2022-09-02 15:46:03 -0300

    primeira modificação

--- src/br/senai/sp/jandira/Calculo.java
+++ src/br/senai/sp/jandira/Calculo.java
@@ -1,16 +1,20 @@
 package br.senai.sp.jandira;
 
 public class Calculo {
+	
 
-	public int multiplicador;
-	public int minimo;
-	public int maximo;
+	public int multiplicador = 8;
+	public int minimo = 5;
+	public int maximo = 10;
+	public int resultado = multiplicador * minimo;
 	
 	
 	public void getCalculo () {
 		
-	}
-	
-
-	
+		while(minimo <= maximo) {
+			System.out.println(multiplicador + "x" + minimo + "=" + resultado);
+			minimo++;
+		}
+    
+  }
 }
